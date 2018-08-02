89f44233ca857b9e31d2284a59877604f0b852c2
Brittanyx91 <brittany.m.blakley@gmail.com>
2018-08-02 17:59:44 -0400

    Model - Assignement

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ require_relative '../models/address_book'
       end
     end
 
+    describe "#remove_entry" do
+      it "removes an entry using name, phone_number, and email address" do
+        book = AddressBook.new
+        book.add_entry("Brittany Blakley", "704.914.8024", "brittany.m.blakley@gmail.com")
+
+        name = "Ada Lovelace"
+        phone_number = "010.012.1815"
+        email_address = "augusta.king@lovelace.com"
+        book.add_entry(name, phone_number, email_address)
+
+        expect(book.entries.size).to eq(2)
+        book.remove_entry(name, phone_number, email_address)
+        expect(book.entries.size).to eq(1)
+        expect(book.entries.first.name).to eq("Brittany Blakley")
+    end
+  end
+
    describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
@@ -25,6 +42,7 @@ require_relative '../models/address_book'
 
        expect(book.entries.size).to eq(1)
      end
+
 
      it "adds the correct information to entries" do
        book = AddressBook.new
@@ -36,4 +54,5 @@ require_relative '../models/address_book'
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
- end
+
+end
